cad75cc66046caee760d8fa06747a6a4bacdf6cc
Shane Moo <obries6@rpi.edu>
2017-10-17 13:28:04 -0400

    RR function added, but needs functionality

--- Project1/process_simulator.py
+++ Project1/process_simulator.py
@@ -183,6 +183,63 @@ def SRT(process_list):
     clock += wait
     print_simulator_finish(clock, algo, pq)
 
+def RR(process_list):
+    pli = 0 #process list index
+    clock = 0 #clock
+    wait = 0 #used to pause running_process for wait + 1 time
+    blocked = [] #list of processes that are blcoking
+    ready_q = Queue() #queue of processes in the ready_q
+    running_process = None #the current process that is running
+    algo = "RR" #which algorithm is being used
+
+    print_simulator_start(clock, algo, ready_q)
+    while not done(process_list):
+        #process arrive and are added to the queue
+        while pli < len(process_list) and process_list[pli].arrival_time == clock:
+            ready_q.push(process_list[pli])
+            print_added_to_ready_queue(clock, process_list[pli], ready_q)
+            pli += 1
+
+        if(wait > 0):
+            wait -= 1
+        elif running_process is None: #need to set a running process
+            if not ready_q.is_empty(): #if there are processes in the ready Q
+                wait = 3
+                running_process = ready_q.pop()
+                print_started_using_cpu(clock + 4, running_process, ready_q)
+        else: #there is a running_process
+            if(running_process.current_burst <= 0): #running process has finished bursts
+                if running_process.restart():
+                    #print that the process is blocking I/O
+                    print_cpu_burst_completion(clock, running_process, ready_q)
+                    print_switching_out(clock, running_process, ready_q)
+                    wait = 3
+                    running_process.io_burst += 3 #to add for the wait time
+                    blocked.append(running_process)
+                else:
+                    print_process_terminated(clock, running_process, ready_q)
+                    wait = 3
+                    #print that the process is terminated
+                running_process = None #making sure another process can become running_process
+            else: #run process normally by decrementing current_burst
+                running_process.current_burst -= 1
+
+        #handle blocked list
+        i = 0
+        while i < len(blocked): #looping through blocks
+            if blocked[i].io_burst <= 0: #if I/O is done
+                change_process = blocked.pop(i)
+                ready_q.push(change_process)
+                print_completed_io(clock + 1, change_process, ready_q)
+                continue
+            else: #if I/O burst needs more time
+                blocked[i].io_burst -= 1
+            i += 1
+        clock += 1
+
+    clock += wait
+    print_simulator_finish(clock, algo, ready_q)
+
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -193,11 +250,15 @@ if __name__ == "__main__":
     f = open(sys.argv[1])
     process_listFCFS = []
     process_listSRT = []
+    process_listRR = []
     for line in f:
         if len(line) == 0 or line[0] == "#": continue
         process_listFCFS.append(Process(line))
         process_listSRT.append(Process(line))
+        process_listRR.append(Process(line))
 
     FCFS(process_listFCFS)
     print()
     SRT(process_listSRT)
+    print()
+    RR(process_listRR)
